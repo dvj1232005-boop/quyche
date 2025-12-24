464ac3213071417dd4e3d7d2ff48587e3b912483
dvj1232005-boop <dvj1232005@gmail.com>
2025-12-24 23:21:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,28 @@
-const CACHE_NAME = "an-quy-che-v2";
-
-const ASSETS = [
+const CACHE_NAME="an-quy-che-v2";
+const ASSETS=[
   "./",
   "./index.html",
   "./manifest.json",
   "./logo.jpg"
 ];
 
-/* INSTALL */
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
-  );
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-/* ACTIVATE */
-self.addEventListener("activate", e => {
+self.addEventListener("activate",e=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
+    caches.keys().then(keys=>
+      Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-/* FETCH */
-self.addEventListener("fetch", e => {
-  if(e.request.method !== "GET") return;
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
   e.respondWith(
-    caches.match(e.request).then(res =>
-      res ||
-      fetch(e.request).then(netRes => {
-        return caches.open(CACHE_NAME).then(c => {
-          c.put(e.request, netRes.clone());
-          return netRes;
-        });
-      })
-    )
+    caches.match(e.request).then(res=>res||fetch(e.request))
   );
 });
