b06c13d0a606f52925d9e877a71ad5788a5d5d29
dvj1232005-boop <dvj1232005@gmail.com>
2025-12-24 21:14:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,5 @@
-const CACHE_NAME = "an-quy-che-v3";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener("install",e=>{
+  self.skipWaiting();
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch",()=>{});
+;
