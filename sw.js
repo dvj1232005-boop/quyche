4b76973e1af86a9cdd4b7ba8f66a8c39cf54378b
dvj1232005-boop <dvj1232005@gmail.com>
2025-12-24 23:13:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,44 @@
-self.addEventListener("install",e=>{
+const CACHE_NAME = "an-quy-che-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./logo.jpg"
+];
+
+/* INSTALL */
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
-self.addEventListener("fetch",()=>{});
-;
+
+/* ACTIVATE */
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+/* FETCH */
+self.addEventListener("fetch", e => {
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(res =>
+      res ||
+      fetch(e.request).then(netRes => {
+        return caches.open(CACHE_NAME).then(c => {
+          c.put(e.request, netRes.clone());
+          return netRes;
+        });
+      })
+    )
+  );
+});
